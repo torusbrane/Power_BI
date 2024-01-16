998c5a91eb18b5686f9b1e2bcdee9b57e4a77c17
Tech Taurus <71295055+torusbrane@users.noreply.github.com>
2024-01-16 15:27:54 -0600

    Adding SQL queries

---
+++ Revenue_for_Jan_2020.sql
@@ -0,0 +1,2 @@
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date = date.date where date.year
+= 2020 and date.month_name = "January" and (transactions.currency = "INR\r" or transactions.currency = "USD\r");
---
+++ Revenue_for_all_2020.sql
@@ -0,0 +1,1 @@
+SELECT SUM(transactions.sales_amount) FROM transactions INNER JOIN date ON transactions.order_date = date.date where date.year = 2020 and transactions.currency = "INR\r" or transactions.currency = "USD\r"
